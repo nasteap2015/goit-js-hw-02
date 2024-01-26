5f38dc0e83bd2c332a1a7d521904a433ff8c18d6
nasteap2015 <149483716+nasteap2015@users.noreply.github.com>
2024-01-26 13:27:14 +0100

    task 1

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 
 function getShippingMessage(country, price, deliveryFee) {
   const totalPrice = price + deliveryFee;
--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 
 function getElementWidth(content, padding, border) {
   return (
